d0501e5bc11fa8a73d5a28325c8eba17a71b3cef
HeShuHeng <1072436890@qq.com>
2020-02-22 12:02:04 +0800

    format the file

--- unit_clause.h
+++ unit_clause.h
@@ -5,8 +5,6 @@
 
 clause_status check_clause(cnf_node current_node, int num); //num means the number of clauses
 solution_status check_matrix(cnf_node &current_node);//check the unit and empty clauses and do the solution
-
-
 status update_by_unit(cnf_node &current_node, int literal);
 
 #endif // !UNIT_CLAUSE_H
