483d200dbb7f8c1e5ef7ac7db7c93d10d1b21109
HeShuHeng <1072436890@qq.com>
2020-03-15 18:10:00 +0800

    head_file propogation

--- propogation.h
+++ propogation.h
@@ -2,5 +2,7 @@
 #define PROPAGATION_H
 
 #include"generate_sodoku.h"
+puzzel_node swap_row(puzzel_node original_node, int row_1, int row_2);
+puzzel_node swap_column(puzzel_node original_node, int column_1, int column_2);
 
 #endif // !PROPAGATION_H
